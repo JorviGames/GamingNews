e9305d94fb36aa79b37bcc8fb9a97c5749c09463
JorviGames <jorviaviles@gmail.com>
2023-09-29 21:41:59 -0600

    Creacion del proyecto

---
+++ src/main/java/com/PaginaGames/PaginaGamesApplication.java
@@ -0,0 +1,13 @@
+package com.PaginaGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PaginaGames/PaginaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PaginaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
